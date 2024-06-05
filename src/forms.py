553cdbdd5300b26ab27d111ea9151130cf3ac57e
NandoSchlemper <haisekun01@gmail.com>
2024-06-04 21:23:35 -0300

    NhecoNheco

--- src/forms.py
+++ src/forms.py
@@ -4,6 +4,6 @@ import wtforms.validators as val
 
 class InserirTicket(FlaskForm):
     ticket = wt.IntegerField('Insira o valor do ticket', validators=[val.DataRequired(), val.Length(1, 25)])
-    data = wt.DateField('Insira a data da descarga', validators=[val.DataRequired(), val.Length(8, 12)])
+    data = wt.StringField('Insira a data da descarga', validators=[val.DataRequired(), val.Length(8, 12)])
     peso = wt.FloatField('Insira o peso da NF', validators=[val.DataRequired()])
     submit = wt.SubmitField('Subimit')
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, redirect
+from flask import Flask, request, jsonify, redirect, render_template
 import sqlite3 as sql
 
 # Vamos comecar a utilizar alguns forms do Flask para facilitar a aplicacao de rodar, segue exemplos:
@@ -6,8 +6,9 @@ from forms import InserirTicket
 
 import secrets
 app = Flask(__name__)
-foo = secrets.token_urlsafe(16)
-app.secret_key(foo)
+#foo = secrets.token_urlsafe(50) # Gerando o token
+#app.secret_key(foo) # Configurando o token na aplicação
+app.secret_key = "NhecoNhecoNheco"
 
 DATABASE_PATH = "form-db.db"
 
@@ -32,23 +33,26 @@ def get_all_tickets():
 
 
 # POST --> Criando novo ticket
-@app.route("/create_ticket", methods=['POST'])
+@app.route("/create_ticket", methods=['POST', 'GET'])
 def create_ticket():
     form = InserirTicket()
     if request.method == "POST":
         if form.validate_on_submit():
-            form_data = form.data
-            print(f"{form_data}")
+            nota_fiscal = form.ticket.data
+            data = form.data.data
+            peso = form.peso.data
             # Fazer a logica por tras do DB e do form_data
             # https://www.geeksforgeeks.org/flask-wtf-explained-how-to-use-it/
             # Ver integração do WSL forms com o HTML
+
+
             con = sql.connect(DATABASE_PATH)
             cur = con.cursor()
-            cur.execute("INSERT INTO tickets (NF, DATA, PESO) VALUES (?, ?, ?)", (nf, date, peso))
+            cur.execute("INSERT INTO tickets (NF, DATA, PESO) VALUES (?, ?, ?)", (nota_fiscal, data, peso))
             con.commit()
             con.close()
-            return jsonify({"message": f"Ticket {nf} added successfully!"}), 201
-    return jsonify({"error": "Request must be JSON"}), 400
+            return f'Ticket: {nota_fiscal} < br > Data: {data} < br > Peso: {peso}'
+    return render_template("index.html")
 
 
 # POST --> Editando completamente um ticket, adicionar edicoes parciais depois
