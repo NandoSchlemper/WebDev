a70b84c2f945406b8064e931ec369910d0680c3e
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-06-06 23:32:25 -0300

    Adicionando formularios e tabelas de Placas e Motoristas, fazendo relacionamentos entre os mesmos

--- src/forms.py
+++ src/forms.py
@@ -2,19 +2,23 @@ from flask_wtf import FlaskForm
 import wtforms as w
 
 class TicketForm(FlaskForm):
-    nfe  = w.IntegerField('Nota Fiscal', validators=[w.validators.DataRequired()])
-    placa   = w.StringField('Placa', validators=[w.validators.DataRequired()])
-    tara    = w.FloatField('Peso tara', validators=[w.validators.DataRequired()])
-    peso_total   = w.FloatField('Peso total carregado', validators=[w.validators.DataRequired()])
-    excesso = w.FloatField('Excesso calculado', validators=[w.validators.DataRequired()])
+    nfe = w.IntegerField('Nota Fiscal', validators=[w.validators.DataRequired()])
+    placa = w.StringField('Placa', validators=[w.validators.DataRequired(), w.validators.Length(min=7, max=7)])
+    tara = w.FloatField('Peso Tara', validators=[w.validators.DataRequired()])
+    peso_total = w.FloatField('Peso Total Carregado', validators=[w.validators.DataRequired()])
+    excesso = w.FloatField('Excesso Calculado', validators=[w.validators.DataRequired()])
 
     submit = w.SubmitField('Submit')
 
 
 class PlacasForm(FlaskForm):
-    placa = w.StringField('Placa', validators=[w.validators.DataRequired()])
+    placa = w.StringField('Placa', validators=[w.validators.DataRequired(), w.validators.Length(min=7, max=7)])
     motorista = w.StringField('Motorista', validators=[w.validators.DataRequired()])
+
+    submit = w.SubmitField('Submit')
 
 
 class MotoristasForm(FlaskForm):
-    motorista = w.StringField('Motorista', validators=[w.validators.DataRequired()])
+    nome = w.StringField('Motorista', validators=[w.validators.DataRequired(), w.validators.Length(max=55)])
+
+    submit = w.SubmitField('Submit')
--- src/models.py
+++ src/models.py
@@ -1,23 +1,21 @@
 from src import db
 
+
 class Tickets(db.Model):
     id         = db.Column(db.Integer, primary_key=True,autoincrement=True)
     nfe        = db.Column(db.Integer, nullable=False)
-    placa      = db.Column(db.String(7), nullable=False)
+    placa      = db.Column(db.String, db.ForeignKey('Placas.placa'), nullable=False)
+    placa_relation      = db.relationship('Placas', blackref='tickets', lazy=True)
     tara       = db.Column(db.Float, nullable=False)
     peso_total = db.Column(db.Float, nullable=False)
     excesso    = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'< Ticket: {self.nfe}, Placa: {self.placa}'
-    
-
 class Placas(db.Model):
     id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
     placa = db.Column(db.String(7), nullable=False)
-    motorista = db.relationship('Motoristas', blackref='nome_motorista', lazy=True)
-
+    motorista = db.Column(db.String, db.ForeignKey('Motoristas.nome'))# So pode ter 1 mot p/cada placa
+    motorista_relation = db.relationship('Motoristas', backref=db.backref('placa', uselist=False))
 
 class Motoristas(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(50), db.ForeignKey('nome'), nullable=False)
+    nome = db.Column(db.String(55), nullable=False)
